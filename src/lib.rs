26e1763d0211522386a8dbd68dcf2ab5ce810fb2
Shogo_Kitada <shogo.kitada@outlook.jp>
2022-08-12 00:14:21 +0900

    Ry

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ mod back_of_house {
 
 pub fn eat_at_restaurant() {
 
-    let mut meal = back_of_house::Breakfast::summer("Ryeee");
+    let mut meal = back_of_house::Breakfast::summer("Ry");
 
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
